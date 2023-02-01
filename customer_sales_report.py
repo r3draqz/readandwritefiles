c705300b6ece79aa1c3efb13099f48ce2488a125
r3draqz <raquael_williams1@baylor.edu>
2023-02-01 14:14:00 -0600

    sales report THE CORRECT WAY

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -15,42 +15,19 @@ writer = csv.writer(outfile, delimiter=',')
 field_name = next(reader)
 
 
-CustomerID = 0
-gtotal = 0
+CustomerID = '250'
+cust_total = 0
 
 # label row
 
 for row in reader:
-    # OrderDate = row[1]
-    # ShipDate = row[2]
+
+    if CustomerID != row[0]:
+        outfile.write(CustomerID + ',' + format(cust_total, '.2f') + '\n')
+        cust_total = 0
+        CustomerID = row[0]
     SubTotal = float(row[3])
     TaxAmt = float(row[4])
     Freight = float(row[5])
-    sales_report = [CustomerID, gtotal]
-
-    Total = SubTotal + TaxAmt + Freight
-    # if int(row[0]) == CustomerID:
-
-    # gtotal += Total
-    # sales_report = [CustomerID, gtotal]
-    # writer.writerow(sales_report)
-    # CustomerID += 1
-
-    if CustomerID != row[0]:
-        writer.writerow(sales_report)
-        Total = 0
-        CustomerID = row[0]
-
-    gtotal += Total
-'''  
-    if int(row[0]) != CustomerID:
-
-        sales_report = [CustomerID, gtotal]
-        writer.writerow(sales_report)
-        gtotal = Total
-        CustomerID += 1
-
-    Total = SubTotal + TaxAmt + Freight
-'''
-
-# Total = 0
+    total = SubTotal + TaxAmt + Freight
+    cust_total += total
